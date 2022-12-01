3d04aa3ebcbe896001678e8630345d6b3ecb7914
Georgina <georgina.scanlon3@mail.dcu.ie>
2022-12-01 18:20:19 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MovieCard from './MovieCard';
+import './App.css';
+import SearchIcon from './search.svg';
+
+// API KEY =  c8626d5d
+
+const API_URL =  'http://www.omdbapi.com/?i=tt3896198&apikey=c8626d5d';
+
+const App = () => {
+    const [movies, setMovies ] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // Search for movie using OMDB API, store in movies array
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json()
+
+        setMovies(data.Search);
+    }
+
+    // Starting search when page loads
+    useEffect(() => {
+        searchMovies('batman');
+    }, []);
+
+    return (
+        <div className='app'>
+            <h1>MovieLookup</h1>
+
+            {/* Search bar. Call searchMovies function with term typed in search bar
+             when search icon clicked */}
+            <div className='search'>
+                <input
+                    placeholder='Search for a movie'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt='searchbar'
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {/* Go through each movie and load card from moviecard componenet
+            If movies array is empty, state no movies found*/}
+            {movies?.length > 0
+                ? (
+                    <div className='container'>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='empty'>
+                        <h2> No movies found</h2>
+                    </div>
+                )}
+        </div>
+    );
 }
 
 export default App;
